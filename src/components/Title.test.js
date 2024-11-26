e1097a963aa2e9490be5394a7686bf73ac508f1d
Dmitry Dmitriev <d.dmitrieff@gmail.com>
2024-11-26 15:30:08 +0300

    upd

--- src/components/Title.test.js
+++ src/components/Title.test.js
@@ -2,17 +2,10 @@ import React from "react"
 import { render } from "@testing-library/react"
 import { Title } from "./Title"
 
-describe("Title", () => {
-    test("Displays the correct title1", () => {
-        const { getByTestId } = render(<Title />)
-        // Assertion
-        expect(getByTestId("hero-title")).toHaveTextContent("Gatsby is awesome!")
-        // --> Test will pass
-    })
-    test("Displays the correct title2", () => {
-        const { getByTestId } = render(<Title />)
-        // Assertion
-        expect(getByTestId("hero-title")).toHaveTextContent("Gatsby is awesome!")
-        // --> Test will pass
-    })
+
+test("Displays the correct title", () => {
+    const { getByTestId } = render(<Title />)
+    // Assertion
+    expect(getByTestId("hero-title")).toHaveTextContent("Gatsby is awesome!")
+    // --> Test will pass
 })
