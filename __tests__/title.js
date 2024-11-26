a77f22324c7a83ea2c2c88aaa0f09c877cf25724
Dmitry Dmitriev <d.dmitrieff@gmail.com>
2024-11-26 13:37:15 +0300

    split test and build

--- __tests__/title.js
+++ __tests__/title.js
@@ -7,6 +7,6 @@ const Title = () => <h1 data-testid="hero-title">Gatsby is awesome!</h1>
 test("Displays the correct title", () => {
     const { getByTestId } = render(<Title />)
     // Assertion
-    expect(getByTestId("hero-title")).toHaveTextContent("Gatsby is awesome!")
+    expect(getByTestId("hero-title")).toHaveTextContent("Gatsby is awesome!!")
     // --> Test will pass
 })
