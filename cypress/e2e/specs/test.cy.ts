253c856c755ff1e259ccf676cae1bab82a77b655
Dmitry Dmitriev <d.dmitrieff@gmail.com>
2024-11-27 15:36:12 +0300

    test fail e2e

--- cypress/e2e/specs/test.cy.ts
+++ cypress/e2e/specs/test.cy.ts
@@ -4,7 +4,7 @@ describe('empty spec', () => {
         cy.visit('http://localhost:9000')
     })
     it('Has title', () => {
-        cy.get('title').should('have.text', 'Home Page')
+        cy.get('title').should('have.text', 'Home Page 1')
     })
     it('Has content', () => {
         cy.get('main').should('have.text', 'Hello Gatsby !')
