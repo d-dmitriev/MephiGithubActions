67c552af2e339e99fb19c537e480f7695264ae85
Dmitry Dmitriev <d.dmitrieff@gmail.com>
2024-11-27 15:24:21 +0300

    e2e

---
+++ cypress/e2e/specs/test.cy.ts
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+describe('empty spec', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:9000')
+    })
+    it('Has title', () => {
+        cy.get('title').should('have.text', 'Home Page')
+    })
+    it('Has content', () => {
+        cy.get('main').should('have.text', 'Hello Gatsby !')
+    })
+})
