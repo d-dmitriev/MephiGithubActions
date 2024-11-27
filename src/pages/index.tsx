d68c1953f710934b7aa84b215d5d217101eb831b
Dmitry Dmitriev <d.dmitrieff@gmail.com>
2024-11-27 18:40:09 +0300

    Fail test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import type { HeadFC, PageProps } from "gatsby";
 
 const IndexPage: React.FC<PageProps> = () => {
-  return <main>Hello Gatsby !</main>;
+  return <main>Hello Gatsby !!!</main>;
 };
 
 export default IndexPage;
